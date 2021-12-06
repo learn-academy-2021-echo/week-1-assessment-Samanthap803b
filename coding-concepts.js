5a887b0b13d79f1c0aa50cca6adc80bb3f5d8345
Samanthap803b <samanthap803b@gmail.com>
2021-12-05 22:56:13 -0800

    Finish my assigment

--- coding-concepts.js
+++ coding-concepts.js
@@ -9,9 +9,9 @@
 
 var cohort = "Echo 2021"
 // console.log(cohort.length)
+// a) Your answer: I think that im going to get a number 8 because is the sice of the string
+// b) Verify and explain: Well I didnt get the correct answer because I start counting with 0 and forget that is not the index but length but now im going to keep this in mind.
 
-// a) Your answer:
-// b) Verify and explain:
 
 
 // --------------------2) What will this log?
@@ -19,18 +19,18 @@ var cohort = "Echo 2021"
 var greeting = "Hello World!"
 // console.log(greeting[3])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I think that is going to appear just "l" because is in that spot but i am still a little confused if with this is the length or index are we starting from 0 or 1.
+// b) Verify and explain: I got it rigth but just because the spot 2 and 3 are the same.
 
 
 // --------------------3) What will this log?
 
 var languages = ["JavaScript", "Ruby", "Python", "C++"]
 var index = 1
-// console.log(languages[index])
+ // console.log(languages[index])
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer:This is saying that from this array the only name that they want is in the number 1 of the index and i think that is "Ruby"
+// b) Verify and explain: I got it rigth and I did undestand.
 
 
 // --------------------4) What will this log?
@@ -38,14 +38,14 @@ var index = 1
 var weekendDays = ["saturday", "sunday"]
 // console.log(weekendDays.toUpperCase())
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: "SATURDAY", "SUNDAY"
+// b) Verify and explain:I try to run the code in my terminal but is not working I get type error.
 
 
 // --------------------5) What will this log?
 
 var dataTypes = ["number", "string", "Boolean"]
-// console.log(typeof dataTypes.length)
+ // console.log(typeof dataTypes.length)
 
-// a) Your answer:
-// b) Verify and explain:
+// a) Your answer: I dont know if is going to be a number or "number" but one of those two.
+// b) Verify and explain: It was "number" but i still dont know if is because we are loocking for a number or why?.
